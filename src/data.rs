70733a6f54dadefaae94b0679672c265982faf11
Byron Wasti <byron.wasti@gmail.com>
2019-08-22 20:09:23 -0400

    Implement a recursive expression evaluator

--- src/data.rs
+++ src/data.rs
@@ -1,4 +1,4 @@
-use crate::parse::{parse, Operation};
+use crate::parse::{parse, LangError};
 use crate::viewer::Item;
 use log::debug;
 use std::collections::HashMap;
@@ -27,39 +27,16 @@ impl Data {
 
     pub fn insert(&mut self, location: CellPos, value: String) {
         if value.starts_with("=") {
-            if let Ok((c1, c2, op)) = parse(&value) {
-                debug!("{:?} {:?} {:?}", &c1, &op, &c2);
-                let c1 = CellPos::new(c1.0, c1.1);
-                let c2 = CellPos::new(c2.0, c2.1);
-
-                self.update_dag(location, &[c1, c2]);
-
-                let c1 = self.get(c1);
-                let c2 = self.get(c2);
-
-
-                if let (Some(c1), Some(c2)) = (c1, c2) {
-                    if let (Ok(c1), Ok(c2)) = (c1.parse::<i32>(), c2.parse::<i32>()) {
-                        let val = match op {
-                            Operation::Mul => c1 * c2,
-                            Operation::Add => c1 + c2,
-                            Operation::Sub => c1 - c2,
-                            Operation::Div => c1 / c2,
-                        };
-                        self.calculated.insert(location, val.to_string());
-                    } else {
-                        self.calculated.insert(location, "#CELL_ERR".to_string());
-                    }
-                } else {
-                    self.calculated.insert(location, "#REF_ERR".to_string());
-                }
-            } else {
-                self.calculated.insert(location, "#PARSE_ERR".to_string());
-            }
-        } else {
-            self.calculated.remove(&location);
+             let val = parse(&value, &self);
+             match val {
+                 Ok(val) => {
+                     self.calculated.insert(location, val.to_string());
+                 },
+                 Err(e) => {
+                     self.calculated.insert(location, e.to_string());
+                 }
+             }
         }
-
         self.cell_data.insert(location, value);
     }
 
--- src/parse.rs
+++ src/parse.rs
@@ -1,59 +1,107 @@
 use log::debug;
-use pest::iterators::Pair;
+use std::fmt;
+use pest::iterators::{Pair, Pairs};
 use pest::Parser;
 use pest_derive::Parser;
+use crate::data::Data;
+use crate::position::CellPos;
 
 #[derive(Parser)]
 #[grammar = "lang.pest"]
 pub struct LangParser;
 
 #[derive(Debug)]
-pub enum ParsingError {
-    Bad,
+pub enum LangError {
+    ParseError,
+    OpError,
+    CellError,
+    RefError,
+}
+
+impl fmt::Display for LangError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use LangError::*;
+        let val = match self {
+            ParseError => "#PARSE_ERR",
+            OpError => "#OP_ERR",
+            CellError => "#CELL_ERR",
+            RefError => "#REF_ERR",
+        };
+        write!(f, "{}", val)
+    }
 }
 
 #[derive(Debug, PartialEq)]
-pub enum Operation {
+enum Operation {
     Div,
     Mul,
     Sub,
     Add,
 }
 
-pub fn parse(source: &str) -> Result<((usize, usize), (usize, usize), Operation), ParsingError> {
+pub fn parse(source: &str, data: &Data) -> Result<i32, LangError> {
     debug!("Source string: {}", source);
-    let mut ident = LangParser::parse(Rule::ident, source).map_err(|_| ParsingError::Bad)?;
-    let p = ident.next().unwrap();
-    let c1 = convert_pair_to_cell_coord(p);
-
-    let p = ident.next().unwrap();
-    let op = convert_pair_to_operation(p);
-
-    let p = ident.next().unwrap();
-    let c2 = convert_pair_to_cell_coord(p);
-
-    Ok((c1, c2, op))
+    let mut ident = LangParser::parse(Rule::ident, source).map_err(|_| LangError::ParseError)?;
+    handle_expression(ident.next().unwrap().into_inner(), data)
 }
 
-pub fn convert_pair_to_cell_coord(pair: Pair<Rule>) -> (usize, usize) {
+pub fn handle_expression(mut pairs: Pairs<Rule>, data: &Data) -> Result<i32, LangError> {
+    let op = pairs.next().unwrap();
+    debug!("{}", &op);
+    let op = match op.as_str() {
+        "add" => Ok(Operation::Add),
+        "sub" => Ok(Operation::Sub),
+        "div" => Ok(Operation::Div),
+        "mul" => Ok(Operation::Mul),
+        n => {
+            debug!("Invalid Op: {}", n);
+            Err(LangError::OpError)
+        }
+    }?;
+
+    let mut vals = Vec::new();
+    for (idx, term) in pairs.enumerate() {
+
+        let val = match term.as_rule() {
+            Rule::cell => {
+                let coord = convert_pair_to_cell_coord(term);
+                let val = data.get(coord).ok_or(LangError::RefError)?;
+                val.parse::<i32>().map_err(|_| LangError::RefError)?
+            }
+            Rule::int => term.as_str().parse::<i32>().map_err(|_| LangError::CellError)?,
+            Rule::expr => handle_expression(term.into_inner(), data)?,
+            n => {
+                debug!("Unexpected Rule: {:?}", n);
+                return Err(LangError::ParseError);
+            }
+            
+        };
+        vals.push(val);
+    }
+
+    if (op == Operation::Sub || op == Operation::Div) && vals.len() > 2 {
+        debug!("Too many values");
+        return Err(LangError::OpError);
+    }
+
+    let out = match op {
+        Operation::Add => vals.iter().sum(),
+        Operation::Mul => vals.iter().product(),
+        Operation::Sub => vals[0] - vals[1],
+        Operation::Div => vals[0] / vals[1],
+    };
+
+    Ok(out)
+}
+
+pub fn convert_pair_to_cell_coord(pair: Pair<Rule>) -> CellPos {
     let mut pair = pair.into_inner();
     let alpha = pair.next().unwrap().as_str();
     let number = pair.next().unwrap().as_str();
     let coord_0 = convert_alpha_str_to_num(alpha);
     let coord_1: usize = number.parse().unwrap();
     let coord_1 = coord_1;
-    return (coord_0, coord_1);
-}
-
-pub fn convert_pair_to_operation(pair: Pair<Rule>) -> Operation {
-    let mut pair = pair.into_inner().next().unwrap();
-    match pair.as_rule() {
-        Rule::div => Operation::Div,
-        Rule::mul => Operation::Mul,
-        Rule::add => Operation::Add,
-        Rule::sub => Operation::Sub,
-        _ => unreachable!(),
-    }
+    return CellPos::new(coord_0, coord_1);
 }
 
 pub fn convert_alpha_str_to_num(alpha: &str) -> usize {
